aab44e11ec0f748df0a9caf40769f9d68fd11320
Rajin Khan <rajin.khan2001@gmail.com>
2024-09-22 19:56:44 +0600

    Add relevant import statements

--- dice_roller/lib/main.dart
+++ dice_roller/lib/main.dart
@@ -1,38 +1,13 @@
 import 'package:flutter/material.dart';
 
+import 'package:dice_roller/gradient_container.dart';
+
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
         body: GradientContainer(),
       ),
     ),
   );
-}
-
-class GradientContainer extends StatelessWidget {
-  @override
-  Widget build(context) {
-    return Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromRGBO(255, 236, 179, 1),
-                Color.fromRGBO(219, 173, 57, 1),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello World!',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 28,
-              ),
-            ),
-          ),
-        );
-  }
 }
