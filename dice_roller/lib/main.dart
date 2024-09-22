924846461a1e14ed6ca5cdfda224113b38cef857
Rajin Khan <rajin.khan2001@gmail.com>
2024-09-22 19:18:30 +0600

    Learn about widgets and widget trees

--- dice_roller/lib/main.dart
+++ dice_roller/lib/main.dart
@@ -2,10 +2,28 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromRGBO(255, 236, 179, 1),
+                Color.fromRGBO(219, 173, 57, 1),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello World!',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 28,
+              ),
+            ),
+          ),
         ),
       ),
     ),
