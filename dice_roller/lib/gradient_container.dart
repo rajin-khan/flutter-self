47b317e9da122a11ed6ee9abf1c0622f45b03a01
Rajin Khan <rajin.khan2001@gmail.com>
2024-09-25 11:18:37 +0600

    Add image assets

--- dice_roller/lib/gradient_container.dart
+++ dice_roller/lib/gradient_container.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 
-import 'package:dice_roller/styled_text.dart';
+//import 'package:dice_roller/styled_text.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer(this.color1, this.color2, {super.key});
+
+  // const GradientContainer.amber({super.key}) //alt constructor function syntax, so we can call GradientContainer.amber(); to call this swatch.
+  //     : color1 = const Color.fromARGB(255, 253, 193, 13),
+  //       color2 = const Color.fromARGB(255, 255, 211, 81);
 
   final Color color1;
   final Color color2;
@@ -21,8 +25,11 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: const Center(
-        child: StyledText('Hello World!'),
+      child: Center(
+        child: Image.asset(
+          'assets/images/dice-5.png',
+          width: 200,
+        ),
       ),
     );
   }
