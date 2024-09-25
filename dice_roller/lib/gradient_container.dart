356695508068f927d73bfed0a07d340bcb972d45
Rajin Khan <rajin.khan2001@gmail.com>
2024-09-25 09:58:35 +0600

    Modify class to accept parameterized input

--- dice_roller/lib/gradient_container.dart
+++ dice_roller/lib/gradient_container.dart
@@ -2,23 +2,26 @@ import 'package:flutter/material.dart';
 
 import 'package:dice_roller/styled_text.dart';
 
+const startAlignment =  Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration:  const BoxDecoration(
         gradient: LinearGradient(
           colors: [
             Color.fromRGBO(255, 236, 179, 1),
             Color.fromRGBO(219, 173, 57, 1),
           ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
       child: const Center(
-        child: StyledText(),
+        child: StyledText('Hello World!'),
       ),
     );
   }
