d490ebf7fda7f2b83c523c6fd35da52cb7571f1c
Rajin Khan <rajin.khan2001@gmail.com>
2024-09-26 21:57:19 +0600

    Modify and add import statements

---
+++ quiz_app/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/gradient_container.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(
+          Colors.blueGrey,
+          Colors.blueGrey.shade100,
+        ),
+      ),
+    ),
+  );
+}
