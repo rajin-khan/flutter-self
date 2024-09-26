538ba3fdef85c1acc2597c70d73778aae6cf8eab
Rajin Khan <rajin.khan2001@gmail.com>
2024-09-26 21:58:35 +0600

    Modify line position

--- dice_roller/lib/gradient_container.dart
+++ dice_roller/lib/gradient_container.dart
@@ -7,11 +7,12 @@ const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer(this.color1, this.color2, {super.key});
 
   // const GradientContainer.amber({super.key}) //alt constructor function syntax, so we can call GradientContainer.amber(); to call this swatch.
   //     : color1 = const Color.fromARGB(255, 253, 193, 13),
   //       color2 = const Color.fromARGB(255, 255, 211, 81);
+
+  const GradientContainer(this.color1, this.color2, {super.key});
 
   final Color color1;
   final Color color2;
