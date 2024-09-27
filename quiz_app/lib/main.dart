b79b40c630d9d36205b469bbc3939195aed7f644
Rajin Khan <rajin.khan2001@gmail.com>
2024-09-27 10:55:34 +0600

    Modify files to make code more concise

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/gradient_container.dart';
+import 'package:quiz_app/welcome_screen.dart';
 
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: GradientContainer(
-          Colors.blueGrey,
-          Colors.blueGrey.shade100,
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.blueGrey,
+                Colors.blueGrey.shade100,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const WelcomeScreen(),
         ),
       ),
     ),
