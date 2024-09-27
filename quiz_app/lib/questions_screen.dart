b81a6f0a36d7d5fe851fc56a22cd140928360b64
Rajin Khan <rajin.khan2001@gmail.com>
2024-09-27 22:37:36 +0600

    Generate answer buttons dynamically

--- quiz_app/lib/questions_screen.dart
+++ quiz_app/lib/questions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -12,36 +13,24 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
+    final currentQuestion = questions[0];
+
     return SizedBox(
       //you could have wrapped the column with center and set the main axis size to min, but this is an alternate way to do it.
       width: double.infinity, //the sizedbox takes up all the available width
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            'The Question...',
-            style: TextStyle(
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(
               color: Colors.white,
-              fontSize: 20,
             ),
           ),
           const SizedBox(height: 30),
-          AnswerButton(
-            'Answer 1',
-            () {},
-          ),
-          AnswerButton(
-            'Answer 2',
-            () {},
-          ),
-          AnswerButton(
-            'Answer 3',
-            () {},
-          ),
-          AnswerButton(
-            'Answer 4',
-            () {},
-          ),
+          ...currentQuestion.answers.map((answer) {
+            return AnswerButton(answer, () {});
+          })
         ],
       ),
     );
