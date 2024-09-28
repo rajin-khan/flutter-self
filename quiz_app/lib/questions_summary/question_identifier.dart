44328c61f35ede56231b7034f2d31788c276b5b3
Rajin Khan <rajin.khan2001@gmail.com>
2024-09-28 22:36:24 +0600

    Finish formatting summary section

---
+++ quiz_app/lib/questions_summary/question_identifier.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class QuestionIdentifier extends StatelessWidget {
+  const QuestionIdentifier(this.isCorrectAnswer, this.questionIndex, {super.key});
+
+  final int questionIndex;
+  final bool isCorrectAnswer;
+
+  @override
+  Widget build(context) {
+    final questionNumber = questionIndex + 1;
+    return Container(
+      width: 30,
+      height: 30,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: isCorrectAnswer
+            ? const Color.fromARGB(255, 37, 234, 86)
+            : const Color.fromARGB(255, 255, 102, 88),
+        borderRadius: BorderRadius.circular(100),
+      ),
+      child: Text(
+        questionNumber.toString(),
+        style: const TextStyle(
+          fontWeight: FontWeight.bold,
+          color: Color.fromARGB(255, 22, 2, 56),
+        ),
+      ),
+    );
+  }
+}
