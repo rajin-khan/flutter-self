75b4737256090661bdb07688841858801f09f4eb
Rajin Khan <rajin.khan2001@gmail.com>
2024-09-28 19:46:36 +0600

    Finish basic results screen and fix answer passed bug

--- quiz_app/lib/questions_summary.dart
+++ quiz_app/lib/questions_summary.dart
@@ -9,9 +9,19 @@ class QuestionsSummary extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: summaryData.map((data) {
-        return Row(children: [
-          Text(((data['question'] as int) + 1).toString())
-        ],);
+        return Row(
+          children: [
+            Text(((data['question_index'] as int) + 1).toString()),
+            Column(
+              children: [
+                Text(data['question'] as String),
+                const SizedBox(height: 5),
+                Text(data['user_answer'] as String),
+                Text(data['correct_answer'] as String),
+              ],
+            ),
+          ],
+        );
       }).toList(),
     );
   }
--- quiz_app/lib/results_screen.dart
+++ quiz_app/lib/results_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/questions_summary.dart';
 
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen(this.chosenAnswers, {super.key});
@@ -32,7 +33,7 @@ class ResultsScreen extends StatelessWidget {
           children: [
             const Text('You answered X out of Y questions correctly!'),
             const SizedBox(height: 30),
-            const Text('List of answers and questions'),
+            QuestionsSummary(getSummaryData()),
             const SizedBox(height: 30),
             TextButton(
               onPressed: () {},
