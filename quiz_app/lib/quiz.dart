3eb9186342eb4a45f8029c96716aefd5b7fa46e0
Rajin Khan <rajin.khan2001@gmail.com>
2024-09-28 11:25:49 +0600

    Add functionality for restarting quiz

--- quiz_app/lib/quiz.dart
+++ quiz_app/lib/quiz.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
 import 'package:quiz_app/questions_screen.dart';
 import 'package:quiz_app/welcome_screen.dart';
 
@@ -13,7 +14,7 @@ class Quiz extends StatefulWidget {
 
 class _QuizState extends State<Quiz> {
   var activeScreen = 'welcome-screen';
-  final List<String> selectedAnswers = [];
+  List<String> selectedAnswers = [];
 
   void switchScreen() {
     setState(() {
@@ -23,6 +24,13 @@ class _QuizState extends State<Quiz> {
 
   void chooseAnswer(String answer) {
     selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        selectedAnswers = [];
+        activeScreen = 'welcome-screen';
+      });
+    }
   }
 
   @override
