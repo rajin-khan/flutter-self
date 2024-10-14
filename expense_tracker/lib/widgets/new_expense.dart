899620ff9866b6af2bcc68a528b266ce91586287
Rajin Khan <rajin.khan2001@gmail.com>
2024-10-14 19:21:05 +0600

    Finish date picker

--- expense_tracker/lib/widgets/new_expense.dart
+++ expense_tracker/lib/widgets/new_expense.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+final formatter = DateFormat.yMd();
 
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
@@ -13,6 +16,22 @@ class _NewExpenseState extends State<NewExpense> {
       TextEditingController(); //creates an object optimised for handling input values
 
   final _amountController = TextEditingController();
+
+  DateTime? _selectedDate;
+
+  void _presentDatePicker() async {
+    final now = DateTime.now();
+    final first = DateTime(now.year - 1, now.month, now.day);
+    final pickedDate = await showDatePicker(
+      context: context,
+      initialDate: now,
+      firstDate: first,
+      lastDate: now,
+    );
+    setState(() {
+      _selectedDate = pickedDate;
+    });
+  }
 
   @override
   void dispose() {
@@ -52,10 +71,12 @@ class _NewExpenseState extends State<NewExpense> {
               const SizedBox(width: 16),
               Expanded(
                 child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    const Text('Selected Date'),
+                    Text(_selectedDate == null ? 'No Date Selected': formatter.format(_selectedDate!)), //with the exclamation mark, we're telling Dart that it won't be a null value.
                     IconButton(
-                      onPressed: () {},
+                      onPressed: _presentDatePicker,
                       icon: const Icon(Icons.calendar_month_rounded),
                     )
                   ],
